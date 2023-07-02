df6a127a350f9b5232f94c41c26a1f429646e26c
Tri Setyobudi <119379263+TriSetyo31@users.noreply.github.com>
2023-07-02 18:30:10 +0700

    Update Salary.py

--- Salary.py
+++ Salary.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 # Load the dataset
 df = pd.read_csv("Salary_Data.csv")
-
-# Split the data into features (X) and target (y)
 X = df.iloc[:, :-1]
 y = df.iloc[:, 1]
 
@@ -14,27 +11,29 @@ y = df.iloc[:, 1]
 regressor = LinearRegression()
 regressor.fit(X, y)
 
-# Create the Streamlit app
-def main():
-    st.title("Salary Prediction")
-    st.markdown("## Years of Experience vs. Salary")
+# Menambahkan judul
+st.title("Salary Prediction")
 
-    # Sidebar inputs
-    years_experience = st.sidebar.number_input("Years of Experience", min_value=0.0, step=0.1)
+# Menambahkan deskripsi atau penjelasan
+st.write("This app predicts the salary based on years of experience.")
 
-    # Make prediction
+# Membuat input field untuk memasukkan tahun pengalaman
+years_experience = st.number_input("Years of Experience", min_value=0, max_value=50, step=1)
+
+# Tombol prediksi
+if st.button("Predict Salary"):
+    # Melakukan prediksi menggunakan model
     salary_pred = regressor.predict([[years_experience]])
 
-    # Display the prediction
-    st.write("The salary for {} years of experience is: $".format(years_experience), salary_pred[0])
+    # Menampilkan hasil prediksi
+    st.write("The predicted salary for {} years of experience is: {}".format(years_experience, salary_pred[0]))
 
-    # Plot the data and regression line
-    plt.scatter(X, y, color="green")
-    plt.plot(X, regressor.predict(X), color="red")
-    plt.title("Years of Experience vs. Salary")
-    plt.xlabel("Years of Experience")
-    plt.ylabel("Salary")
-    st.pyplot()
+# Menampilkan scatter plot dari data
+st.subheader("Scatter Plot")
+st.write("This plot shows the relationship between years of experience and salary.")
+st.scatter_chart(df)
 
-if __name__ == "__main__":
-    main()
+# Menampilkan plot regresi
+st.subheader("Regression Plot")
+st.write("This plot shows the regression line fitted to the data.")
+st.line_chart(df)
